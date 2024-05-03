ada32693f971ac50d6ca318133863bec43b39ccf
dendum <dendumych@gmail.com>
2024-05-03 11:32:44 +0200

    Rewritten main map view (the robots run smoothly)

---
+++ src/tool/view/MapView.java
@@ -0,0 +1,37 @@
+package tool.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class MapView extends JPanel {
+
+    private List<RobotView> robots;
+
+    public MapView(GridLayout var3, List<RobotView> robots) {
+        super(var3);
+        this.robots = robots;
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        int tileWidth = this.getWidth() / 8; // total width / number of columns
+        int tileHeight = this.getHeight() / 5; // total height / number of rows
+
+        g.setColor(Color.BLACK);
+        for (int i = 0; i <= 8; i++) {
+            int x = i * tileWidth;
+            g.drawLine(x, 0, x, getHeight());
+        }
+        for (int i = 0; i <= 5; i++) {
+            int y = i * tileHeight;
+            g.drawLine(0, y, getWidth(), y);
+        }
+
+        for (RobotView robot : this.robots) {
+            robot.draw(g);
+        }
+    }
+}
